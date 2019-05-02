d0da11d60d3b9ada95b8fae4f58b11cb3df3fd66
Seiya <seiya.loveless.mail@gmail.com>
2019-05-02 12:20:29 +0300

    fix default user agent

--- hls-help.js
+++ hls-help.js
@@ -7,7 +7,7 @@ const querystring = require('querystring');
 
 // modules
 const got = require('got').extend({
-    headers: { userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:65.0) Gecko/20100101 Firefox/65.0' },
+    headers: { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:65.0) Gecko/20100101 Firefox/65.0' },
 });
 
 // extra
--- ttml-help.js
+++ ttml-help.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // modules
 const got = require('got').extend({
-    headers: { userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:65.0) Gecko/20100101 Firefox/65.0' },
+    headers: { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:65.0) Gecko/20100101 Firefox/65.0' },
 });
 
 // extra
